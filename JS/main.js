33f1ff3e4e8c3f7299518bded132e8732fbdecd7
aqdtjjksytjksfyksk <142991426+SamatovAbdulaziz@users.noreply.github.com>
2023-08-26 13:53:01 +0500

    JS 1-dars
    26-guruh o'quvchisi

---
+++ JS/main.js
@@ -0,0 +1,69 @@
+/*console.log("5*5+5-5/5");*/
+/*alert("Saytimizga xush kelibsiz!")*/
+/*let x = 2;*/
+/*let y = 5;*/
+/*x = 10;*/
+/*let x = 2, y = 5, z = 12;*/
+/*let rang = "#f1f1f1";*/
+/*document.write(rang);*/
+
+/*let son = 45; */                     /*number*/
+/*let matn = Dasturlash;      */      /*string*/
+
+let x = 5;
+/*x+=5*/
+/*x-=5;*/
+/*x*=5;*/
+/*x/=5*/
+/*let qoldiq = x%13;*/
+/*let natija = x**5;*/
+/*--x;--x; ("Block!");*/
+/*document.write(x+5);*/
+
+
+/*if (agar)*/
+/* else (u holda)*/
+/* else (yoki bo'lmasa) => shart formulalari*/
+
+/*if (44 == 44){
+    console.log("Ular teng!");
+}*/
+/*else {
+    console.log("Ular teng emas!");
+}*/
+/*
+if (99 === "9"){
+    console.log("teng");
+}
+else {
+    console.log("teng emas");
+}
+*/
+/*if (45 == 44){
+    console.log("Qiymatlari teng! - 1");
+}
+else if (45 == 46){
+    console.log("Qiymatlar teng! - 2");
+}
+else {
+    console.log("Birorta qiymat teng emas!");
+}*/
+let a = 10;
+let b = 50;
+let c = 60;
+let d = 70;
+
+/*
+if (a == 15 && b == 55 && c == 64 && d == 78){
+    console.log("qiymat teng")
+}
+else {
+    console.log("qiymat teng emas")
+}
+*/
+if (a == 10 || b == 50 || c == 60 || d == 70){
+    console.log("==")
+}
+else {
+    console.log("<>")
+}
